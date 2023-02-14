85eea21ee12e77aebfd566669c7142d75ea47ad3
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-14 01:45:22 -0600

    Remove 'index.test.js'

--- src/tinycolor.js
+++ src/tinycolor.js
@@ -4,3 +4,41 @@ added an HTML button with the text "Generate Colors" and the id
 "generate-colors-btn" I added a container div with class 'color-container' to
 hold the generated color boxes. I added some basic CSS to style the button and
 color boxes */
+
+// import tinycolor from "tinycolor2";
+
+// blocker - how to generate a random color without asking the user for a color. Create an onClick event listener to a button that generates random color sets.
+
+import tinycolor from "tinycolor2";
+const color = tinycolor("red");
+
+/// ///////////
+
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+function generateAnalogousColors(color) {
+  const hsl = tinycolor(color).toHsl();
+  const colors = [];
+  for (let i = -1; i <= 1; i++) {
+    const newColor = tinycolor({
+      h: (hsl.h + 30 * i) % 360,
+      s: hsl.s,
+      l: hsl.l,
+    });
+    colors.push(newColor.toHexString());
+  }
+
+  return colors;
+}
+
+const randomColor = generateRandomColor();
+const analogousColors = generateAnalogousColors(randomColor);
+console.log(analogousColors);
