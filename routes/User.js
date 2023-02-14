37a3f8d3f3228c3a233ed73b2580d45c64298db8
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-14 15:58:17 -0600

    Change "router.post" to "router.get"

--- routes/User.js
+++ routes/User.js
@@ -4,13 +4,13 @@ const express = require("express");
 const router = express.Router();
 
 // usually you would do
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("You just created a user");
 });
 
 module.exports = router; // export the router
 
-///////////////////////
+/// ////////////////////
 
 const app = express();
 
@@ -23,14 +23,14 @@ app.listen(3005, () => {
   console.log("Server running on port 3005");
 });
 
-//usually post request to create a user you would do
+// usually post request to create a user you would do
 
 // Initiate the route
 const userRoute = require("./routes/User");
 
 append.use(userRoute, "/user"); // whaterver is write will be executable at localhost:3005/user. Making a POST request to localhost:3005/
 
-//////
+/// ///
 
 /*
 Changed the variable name 'app.listen' to 'append.listen' to avoid confusion with the 'app' variable in the 'express' module
