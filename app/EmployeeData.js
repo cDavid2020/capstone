0add55b537d510cf6388666883ecdc59c43ba33f
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-15 03:33:36 -0600

    Faker data into 'EmployeeData.js'

--- app/EmployeeData.js
+++ app/EmployeeData.js
@@ -9,7 +9,8 @@ lastName: faker.name.LastName(),
 phrase: faker.hacker.phrase(),
 username: faker.internet.userName(),
 });
-
-const users = Array.from({ length: 100 }).map(createFakeUser);
+// create 1000 users with faker in an array
+//const users = Array.from({ length: 100 }, createFakeUser); // create an array of undefined elements.
+const users = Array.from({ length: 100 }).map(createFakeUser);// now its an array i can MAP over each element. The map() function goes over each element of a collection and applies a given function to it, and returns a new collection with the transformed values
 
 fs.writeFile("users.json", JSON.stringify(users, null, 2), "utf8");
--- app/faker-users.js
+++
@@ -1,4 +0,0 @@
-// create 1000 users with faker in an array
-
-const users = Array.from({ length: 100 }, createFakeUser); // create an array of undefined elements.
-const users = Array.from({ length: 100 }).map(createFakeUser); // now its an array i can mak over each element. map goes over each element and .
