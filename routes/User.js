2c8ea74feda77eb18cd958767dc3b71fd2ef93b6
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-14 19:51:44 -0600

    Cleanup 'User.js'

--- routes/User.js
+++ routes/User.js
@@ -8,29 +8,7 @@ const router = express.Router();
 const controller = require("../controllers/UserController");
 
 // import the controller created
-
 router.get("/", controller.get);
 
-module.exports = router; // export the router
-
-/// ////////////////////
-
-/*
-V1 CODE: const { append } = require("express/lib/response");
-const router = express.Router();
- */
-
-// Initiate the route
-const userRoute = require("./routes/User");
-
-append.use(userRoute, "/user"); // whaterver is write will be executable at localhost:3005/user. Making a POST request to localhost:3005/
-
-/// ///
-
-/*
-Changed the variable name 'app.listen' to 'append.listen' to avoid confusion with the 'app' variable in the 'express' module
-
-app.listen(3005, () => {
-  console.log("Server running on port 3005");
-});)
- */
+// export the router
+module.exports = router;
