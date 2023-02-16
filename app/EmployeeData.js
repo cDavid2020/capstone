eddeae089ad28524aaf74a94d139934cf0d5a09d
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-16 01:01:19 -0600

    Remove Faker js

--- app/EmployeeData.js
+++ app/EmployeeData.js
@@ -7,18 +7,6 @@ In Node.js, "fs" is a built-in module that provides an API for working with the 
 
 The statement "import { promises as fs } from 'fs';" is an ES6 import statement that allows you to import the "promises" property of the "fs" module and give it an alias of "fs". This makes it easier to reference the asynchronous file system functions provided by the "fs" module in your code.
  */
-import { promises as fs } from "fs";
-
-const createFakeUser = () => {
-  return {
-    fullName: faker.name.fullName(),
-    companyName: faker.company.companyName()
-    username: faker.internet.userName(),
-});
-// create 1000 users with faker in an array
-//const users = Array.from({ length: 100 }, createFakeUser); // create an array of undefined elements.
-const users = Array.from({ length: 100 }).map(createFakeUser);// now its an array i can MAP over each element. map() function goes over each element of a collection 100 times and applies a given function to it, and returns a new collection with the transformed values
-
 fs.writeFile("users.json", JSON.stringify(users, null, 2), "utf8"); // this takes in a JavaScript object and STRINGIFY the USERS we just created with .writeFile. Take the Array of objects and STRINGIFY into a JSON file readable in UTF8 format.
 
 console.log(createFakeUser());
--- src/index-v2.js
+++ src/index-v2.js
@@ -1,16 +1,3 @@
-// Usage folder only has object arrays?? - https://fakerjs.dev/guide/usage.html
-import { faker } from "@faker-js/faker";
-  return {
-id
-avatar
-fullname
-
-
-
-  }
-const createFakeUser = () => ({
-
-
 // CODE V1: export const add2Nums = (num1, num2) => num1 + num2;
 const express = require("express");
 const app = express();
@@ -21,26 +8,6 @@ app.use("/user", userRoute); // correct 2/14/23
 
 // Previously:
 // app.use(userRoute, "/user");  old 2/14/23
-const faker = require("faker");
-const currentYear = new Date().getFullYear();
-
-const people = faker.helpers.times(100, (n) => {
-  // get the current year
-  const hireDate = faker.date.between(
-    //   // Generate a random hire date between 16 years ago and the beginning of the current year
-    new Date(currentYear - 16, 0, 1), // / Start date is 16 years ago on January 1st
-    new Date(currentYear, 0, 1) //  End date is the beginning of the current year on January 1st
-  );
-
-  return {
-    employeeId: n + 1, // Create an employeeId number starting at one and increment to 100
-    name: {
-      first: faker.name.firstName(),
-      last: faker.name.lastName(),
-    },
-    hireDate,
-  };
-});
 
 // print the contents of the `people` array
 console.log(people);
